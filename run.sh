9af60c1a07cd968d3b01f7ce799b019414b4d841
7zzz-hub <zhuoqihe497@163.com>
2026-08-26 23:05:15 +0800

    .

--- run.sh
+++ run.sh
@@ -1,13 +1,23 @@
-python run_gptq.py \
-  --model_dir models/llama3.1-8b/llama3.1-8b-instruct \
-  --save_dir models/llama3.1-8b/llama3.1-8b-gptq-int4-calib \
+# python run_gptq.py \
+#   --model_dir models/qwen3-8b \
+#   --save_dir models/qwen3-8b/qwen3-8b-gptq-int4-calib \
+#   --calib_file data/quantization/calibration_wikipedia_multilingual.jsonl 
+
+python run_awq.py \
+  --model_dir models/llama3.1-8b \
+  --save_dir models/llama3.1-8b/llama3.1-8b-awq-int4-calib \
   --calib_file data/quantization/calibration_wikipedia_multilingual.jsonl 
 
-python main.py \
-  --dataset_type klar \
-  --checkpoint /root/autodl-tmp/Multilingual_QE/models/llama3.1-8b \
-  --quant_type fp16 \
-  --model_type llama3.1 \
-  --languages ar,ca,el,en,es,fr,he,hu,ja,ko,nl,tr,zh \
-  --batch_size 16 \
-  --save_path results
+python run_awq.py \
+  --model_dir models/qwen3-8b \
+  --save_dir models/qwen3-8b/qwen3-8b-awq-int4-calib \
+  --calib_file data/quantization/calibration_wikipedia_multilingual.jsonl 
+
+# python main.py \
+#   --dataset_type klar \
+#   --checkpoint /root/autodl-tmp/Multilingual_QE/models/llama3.1-8b \
+#   --quant_type fp16 \
+#   --model_type llama3.1 \
+#   --languages ar,ca,el,en,es,fr,he,hu,ja,ko,nl,tr,zh \
+#   --batch_size 16 \
+#   --save_path results
--- run_awq.py
+++ run_awq.py
@@ -18,11 +18,11 @@ def get_args():
     parser.add_argument("--num_languages", type=int, default=13)
     parser.add_argument("--records_per_language", type=int, default=1000)
     parser.add_argument("--samples_per_language", type=int, default=32)
-    parser.add_argument("--seq_len", type=int, default=2048)
+    parser.add_argument("--seq_len", type=int, default=512)
 
     parser.add_argument("--w_bit", type=int, default=4)
     parser.add_argument("--q_group_size", type=int, default=128)
-    parser.add_argument("--zero_point", action="store_true")
+    parser.add_argument("--zero_point", action=argparse.BooleanOptionalAction, default=True)
     parser.add_argument("--version", default="GEMM")
 
     parser.add_argument("--seed", type=int, default=0)
